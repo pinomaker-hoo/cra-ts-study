bc27b0b8e58b9d08a26d6a7cb2cabe4270079005
pinomaker-hoo <56928532+pinomaker-hoo@users.noreply.github.com>
2023-05-11 01:09:28 +0900

    feat : Add Code

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,91 @@
-function App() {
-  return <h1>hello world</h1>;
+import axios from "axios"
+import useInput from "./hooks/useInput"
+import { useEffect, useState } from "react"
+
+const App = () => {
+  const [user, setUser] = useInput({ id: "", password: "" })
+  const [data, setData] = useState<any[]>([])
+  const [reRenderSwitch, setReRenderSwitch] = useState<boolean>(false)
+
+  const handleRefetch = () => setReRenderSwitch(true)
+
+  const handleLogin = async () => {
+    if (user.id === "" || user.password === "") {
+      alert("데이터 입력")!
+
+      return
+    }
+
+    try {
+      const {
+        data: {
+          status,
+          message,
+          responseData: { accessToken },
+        },
+      } = await axios.post(
+        "http://phone.pinodev.shop:8000/api/user/login",
+        user
+      )
+
+      if (status === 200) {
+        localStorage.setItem("token", accessToken)
+
+        alert(message)
+
+        return
+      }
+
+      alert(message)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  const fetchData = async () => {
+    try {
+      const { data } = await axios.get(
+        "http://phone.pinodev.shop:8000/api/phone",
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      )
+
+      return data
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  useEffect(() => {
+    if (reRenderSwitch) {
+      setReRenderSwitch(false)
+    }
+
+    fetchData().then(({ responseData }) => {
+      setData(responseData)
+    })
+  }, [reRenderSwitch])
+
+  return (
+    <div>
+      <h1>LOGIN</h1>
+      <input type="text" value={user.id} onChange={setUser} name="id" />
+      <input
+        type="password"
+        value={user.password}
+        onChange={setUser}
+        name="password"
+      />
+      <button onClick={handleLogin}>LOGIN</button>
+      {data.map((item, index: number) => (
+        <li key={index}>{item.name}</li>
+      ))}
+      <button onClick={handleRefetch}>FETCH</button>
+    </div>
+  )
 }
 
-export default App;
+export default App
